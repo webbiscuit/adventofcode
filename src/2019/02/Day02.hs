b8a4554131ff9ab91f85a45cbf8ff5643ac9ef7c
Daniel <daniel@webbiscuit.co.uk>
2019-12-03 00:01:28 +0000

    Complete part 2

--- src/2019/02/Day02.hs
+++ src/2019/02/Day02.hs
@@ -3,23 +3,22 @@ module Day02
   runProgram
 ) where
 
-import Data.Vector  hiding (map, (++))
+import Data.Vector (Vector, fromList, (!), (//))
 import Data.List.Split
 
 type Source = Int
 type Destination = Int
 type Memory = Vector Int
-data Operation = Add Source Source Destination | Multiply Source Source Destination | End deriving (Show)
+type Noun = Int
+type Verb = Int
+data Instruction = Add Source Source Destination | Multiply Source Source Destination | End deriving (Show)
 
-generateProgram :: [Int] -> Vector Int
-generateProgram = fromList
+executeInstruction :: Instruction -> Memory -> Memory
+executeInstruction (Add s1 s2 d1) memory = memory // [(d1, (memory ! s1) + (memory ! s2))]
+executeInstruction (Multiply s1 s2 d1) memory = memory // [(d1, (memory ! s1) * (memory ! s2))]
 
-executeOperation :: Operation -> Memory -> Memory
-executeOperation (Add s1 s2 d1) memory = memory // [(d1, (memory ! s1) + (memory ! s2))]
-executeOperation (Multiply s1 s2 d1) memory = memory // [(d1, (memory ! s1) * (memory ! s2))]
-
-toOperation :: Int -> Memory -> Operation
-toOperation location memory = toOp $ memory ! location
+toInstruction :: Int -> Memory -> Instruction
+toInstruction location memory = toOp $ memory ! location
   where 
     toOp 1 = Add address1 address2 address3 
     toOp 2 = Multiply address1 address2 address3
@@ -31,13 +30,22 @@ toOperation location memory = toOp $ memory ! location
 runProgram :: Memory -> Memory
 runProgram = runLoop 0
     where 
-      runLoop pos memory = handle (toOperation pos memory)
+      runLoop pos memory = handle (toInstruction pos memory)
         where
           handle End = memory
-          handle op = runLoop (pos + 4) (executeOperation op memory)
+          handle instr = runLoop (pos + 4) (executeInstruction instr memory)
 
 putCode :: Memory -> Destination -> Int -> Memory
 putCode memory destination value = memory // [(destination, value)]
+
+makeProgram :: Memory -> (Noun,Verb) -> Memory
+makeProgram mem (noun,verb) = mem // [(1, noun), (2, verb)]
+
+makeAllPrograms :: Memory -> [Memory]
+makeAllPrograms seedProgram = scanl makeProgram seedProgram [(a,b) | a <- [0..99], b <- [0..99]]
+
+findProgram :: Memory -> Int -> Memory
+findProgram seed goal = head $ filter (\m -> m ! 0 == goal) $ map runProgram $ makeAllPrograms seed
 
 parseInput :: String -> Memory
 parseInput = fromList . map read . splitOn ","
@@ -50,4 +58,8 @@ main = do
   let result = runProgram programAlarm1202 ! 0 
 
   putStrLn ("Value at position 0 is " ++ show result)
-  
+
+  let foundProgram = findProgram program 19690720
+  let nounVerb = (foundProgram ! 1) * 100 + foundProgram ! 2
+
+  putStrLn ("Noun/Verb code for 19690720 is " ++ show nounVerb)

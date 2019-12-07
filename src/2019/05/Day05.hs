3296d6f6b175ba8b1b79409d2576bed7303bb7ab
Daniel <daniel@webbiscuit.co.uk>
2019-12-07 23:59:44 +0000

    Add jumps

--- src/2019/05/Day05.hs
+++ src/2019/05/Day05.hs
@@ -25,6 +25,7 @@ data Mode = Position | Immediate deriving Show
 type Input = Int
 type Output = [Int]
 type OpCode = Int
+type MemoryPosition = Int
 
 executeInstruction :: Instruction -> Memory -> Memory
 executeInstruction (Add s1 s2 d1) memory = memory // [(d1, memoryLookup memory s1 + memoryLookup memory s2)]
@@ -42,14 +43,18 @@ executeInput (PutInput destination) memory input = memory // [(destination, inpu
 executeOutput :: Instruction -> Memory -> Output -> Output
 executeOutput (PutOutput value) memory output = output ++ [memoryLookup memory value]
 
-toOpCodeAndModes :: Int -> Memory -> (OpCode, [Mode])
+executeJump :: Instruction -> Memory -> MemoryPosition -> MemoryPosition
+executeJump (JumpIfTrue p1 p2) memory initialPos = if memoryLookup memory p1 /= 0 then memoryLookup memory p2 else initialPos + 3
+executeJump (JumpIfFalse p1 p2) memory initialPos = if memoryLookup memory p1 == 0 then memoryLookup memory p2 else initialPos + 3
+
+toOpCodeAndModes :: MemoryPosition -> Memory -> (OpCode, [Mode])
 toOpCodeAndModes location memory = (toOpCode, reverse toModes)
   where 
     padOpCode = printf "%05d" (memory ! location) :: String
     toOpCode = read $ drop 3 padOpCode
     toModes = map (\c -> toMode $ read [c]) $ take 3 padOpCode
 
-toInstruction :: Int -> Memory -> Instruction
+toInstruction :: MemoryPosition -> Memory -> Instruction
 toInstruction location memory = toOp opCodeAndModes
   where 
     opCodeAndModes = toOpCodeAndModes location memory
@@ -57,6 +62,8 @@ toInstruction location memory = toOp opCodeAndModes
     toOp (2, [m1,m2,_]) = Multiply (m1, lookup 1) (m2, lookup 2) (lookup 3)
     toOp (3, _) = PutInput $ lookup 1
     toOp (4, m1:_) = PutOutput (m1, lookup 1)
+    toOp (5, [m1,m2,_]) = JumpIfTrue (m1, lookup 1) (m2, lookup 2)
+    toOp (6, [m1,m2,_]) = JumpIfFalse (m1, lookup 1) (m2, lookup 2)
     toOp (7, [m1,m2,_]) = LessThan (m1, lookup 1) (m2, lookup 2) (lookup 3)
     toOp (8, [m1,m2,_]) = Equals (m1, lookup 1) (m2, lookup 2) (lookup 3)
     toOp (99, _) = End
@@ -66,15 +73,17 @@ toMode :: Int -> Mode
 toMode 0 = Position
 toMode 1 = Immediate
 
-runProgram :: Memory -> Int -> (Memory, Output)
+runProgram :: Memory -> MemoryPosition -> (Memory, Output)
 runProgram program input = runLoop 0 program input []
     where 
-      runLoop :: Int -> Memory -> Input -> Output -> (Memory, Output)
+      runLoop :: MemoryPosition -> Memory -> Input -> Output -> (Memory, Output)
       runLoop pos memory input output = handle (toInstruction pos memory)
         where
           handle End = (memory, output)
           handle instr@(PutInput _) = runLoop (pos + 2) (executeInput instr memory input) input output
           handle instr@(PutOutput _) = runLoop (pos + 2) memory input (executeOutput instr memory output)
+          handle instr@(JumpIfTrue _ _) = runLoop (executeJump instr memory pos) memory input output
+          handle instr@(JumpIfFalse _ _) = runLoop (executeJump instr memory pos) memory input output
           handle instr = runLoop (pos + 4) (executeInstruction instr memory) input output
 
 putCode :: Memory -> Destination -> Int -> Memory

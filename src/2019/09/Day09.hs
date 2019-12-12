36c247ba169d4a1bf5ff2e6f40bed64ebf8caf29
Daniel <daniel@webbiscuit.co.uk>
2019-12-12 00:18:51 +0000

    Complete day9

--- src/2019/09/Day09.hs
+++ src/2019/09/Day09.hs
@@ -13,15 +13,15 @@ type Value = Int
 type Destination = Int
 type Memory = Vector Int
 data Instruction = 
-  Add Parameter Parameter Destination | 
-  Multiply Parameter Parameter Destination | 
+  Add Parameter Parameter Parameter | 
+  Multiply Parameter Parameter Parameter | 
   PutInput Parameter |
   PutOutput Parameter |
   JumpIfTrue Parameter Parameter |
   JumpIfFalse Parameter Parameter |
-  LessThan Parameter Parameter Destination |
-  Equals Parameter Parameter Destination |
-  AdjustRelativeBase MemoryPosition |
+  LessThan Parameter Parameter Parameter |
+  Equals Parameter Parameter Parameter |
+  AdjustRelativeBase Parameter |
   End deriving Show
 type Parameter = (Mode, Value)
 data Mode = Position | Immediate | Relative deriving Show
@@ -31,10 +31,14 @@ type OpCode = Int
 type MemoryPosition = Int
 
 executeInstruction :: Instruction -> Memory -> MemoryPosition -> Memory
-executeInstruction (Add s1 s2 d1) memory relativePos = memory // [(d1, memoryLookup memory relativePos s1 + memoryLookup memory relativePos s2)]
-executeInstruction (Multiply s1 s2 d1) memory relativePos = memory // [(d1, memoryLookup memory relativePos s1 * memoryLookup memory relativePos s2)]
-executeInstruction (LessThan s1 s2 d1) memory relativePos = memory // [(d1, if memoryLookup memory relativePos s1 < memoryLookup memory relativePos s2 then 1 else 0)]
-executeInstruction (Equals s1 s2 d1) memory relativePos = memory // [(d1, if memoryLookup memory relativePos s1 == memoryLookup memory relativePos s2 then 1 else 0)]
+executeInstruction (Add s1 s2 d1) memory relativePos = memory // [(destMemoryLookup relativePos d1, memoryLookup memory relativePos s1 + memoryLookup memory relativePos s2)]
+executeInstruction (Multiply s1 s2 d1) memory relativePos = memory // [(destMemoryLookup relativePos d1, memoryLookup memory relativePos s1 * memoryLookup memory relativePos s2)]
+executeInstruction (LessThan s1 s2 d1) memory relativePos = memory // [(destMemoryLookup relativePos d1, if memoryLookup memory relativePos s1 < memoryLookup memory relativePos s2 then 1 else 0)]
+executeInstruction (Equals s1 s2 d1) memory relativePos = memory // [(destMemoryLookup relativePos d1, if memoryLookup memory relativePos s1 == memoryLookup memory relativePos s2 then 1 else 0)]
+
+destMemoryLookup :: MemoryPosition -> Parameter -> Value
+destMemoryLookup _ (Position,address) = address
+destMemoryLookup relativePos (Relative,value) = relativePos + value
 
 memoryLookup :: Memory -> MemoryPosition -> Parameter -> Value
 memoryLookup memory _ (Position,address) = memory ! address
@@ -42,7 +46,7 @@ memoryLookup memory _ (Immediate,value) = value
 memoryLookup memory relativePos (Relative,value) = memory ! (relativePos + value)
 
 executeInput :: Instruction -> Memory -> MemoryPosition -> Input -> Memory
-executeInput (PutInput value) memory relativePos input = memory // [(snd value, input)]
+executeInput (PutInput value) memory relativePos input = memory // [(destMemoryLookup relativePos value, input)]
 
 executeOutput :: Instruction -> Memory -> MemoryPosition -> Output -> Output
 executeOutput (PutOutput value) memory relativePos output = output ++ [memoryLookup memory relativePos value]
@@ -51,8 +55,8 @@ executeJump :: Instruction -> Memory -> MemoryPosition -> MemoryPosition -> Memo
 executeJump (JumpIfTrue p1 p2) memory relativePos initialPos = if memoryLookup memory relativePos p1 /= 0 then memoryLookup memory relativePos p2 else initialPos + 3
 executeJump (JumpIfFalse p1 p2) memory relativePos initialPos = if memoryLookup memory relativePos p1 == 0 then memoryLookup memory relativePos p2 else initialPos + 3
 
-executeAdjustRelativeBase ::Instruction -> MemoryPosition -> MemoryPosition
-executeAdjustRelativeBase (AdjustRelativeBase p) relativePos = relativePos + p
+executeAdjustRelativeBase ::Instruction -> Memory -> MemoryPosition -> MemoryPosition
+executeAdjustRelativeBase (AdjustRelativeBase p) memory relativePos = relativePos + memoryLookup memory relativePos p
 
 toOpCodeAndModes :: MemoryPosition -> Memory -> (OpCode, [Mode])
 toOpCodeAndModes location memory = (toOpCode, reverse toModes)
@@ -65,15 +69,15 @@ toInstruction :: MemoryPosition -> Memory -> Instruction
 toInstruction location memory = toOp opCodeAndModes
   where 
     opCodeAndModes = toOpCodeAndModes location memory
-    toOp (1, [m1,m2,_]) = Add (m1, lookup 1) (m2, lookup 2) (lookup 3) 
-    toOp (2, [m1,m2,_]) = Multiply (m1, lookup 1) (m2, lookup 2) (lookup 3)
+    toOp (1, [m1,m2,m3]) = Add (m1, lookup 1) (m2, lookup 2) (m3, lookup 3) 
+    toOp (2, [m1,m2,m3]) = Multiply (m1, lookup 1) (m2, lookup 2) (m3, lookup 3)
     toOp (3, m1:_) = PutInput (m1, lookup 1)
     toOp (4, m1:_) = PutOutput (m1, lookup 1)
     toOp (5, [m1,m2,_]) = JumpIfTrue (m1, lookup 1) (m2, lookup 2)
     toOp (6, [m1,m2,_]) = JumpIfFalse (m1, lookup 1) (m2, lookup 2)
-    toOp (7, [m1,m2,_]) = LessThan (m1, lookup 1) (m2, lookup 2) (lookup 3)
-    toOp (8, [m1,m2,_]) = Equals (m1, lookup 1) (m2, lookup 2) (lookup 3)
-    toOp (9, _) = AdjustRelativeBase $ lookup 1
+    toOp (7, [m1,m2,m3]) = LessThan (m1, lookup 1) (m2, lookup 2) (m3, lookup 3)
+    toOp (8, [m1,m2,m3]) = Equals (m1, lookup 1) (m2, lookup 2) (m3, lookup 3)
+    toOp (9, m1:_) = AdjustRelativeBase (m1, lookup 1)
     toOp (99, _) = End
     lookup delta = memory ! (location + delta)
 
@@ -101,7 +105,7 @@ resumeProgram program input pos = finalise $ runLoop pos (raiseMemory program 10
         handle instr@(PutOutput _) = runLoop (pos + 2) memory input (executeOutput instr memory relativePos output) relativePos
         handle instr@(JumpIfTrue _ _) = runLoop (executeJump instr memory relativePos pos) memory input output relativePos
         handle instr@(JumpIfFalse _ _) = runLoop (executeJump instr memory relativePos pos) memory input output relativePos
-        handle instr@(AdjustRelativeBase _) = runLoop (pos + 2) memory input output (executeAdjustRelativeBase instr relativePos)
+        handle instr@(AdjustRelativeBase _) = runLoop (pos + 2) memory input output (executeAdjustRelativeBase instr memory relativePos)
         handle instr = runLoop (pos + 4) (executeInstruction instr memory relativePos) input output relativePos
 
 putCode :: Memory -> Destination -> Int -> Memory
@@ -119,11 +123,6 @@ raiseMemory program amount = program V.++ V.replicate amount 0
 packMemory :: Memory -> Memory
 packMemory = V.reverse . V.dropWhile (==0) . V.reverse
 
-p :: Memory
-p = fromList [
-  3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-  27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
-
 main = do
   input <- getLine
 
@@ -131,3 +130,7 @@ main = do
   let boostResult = runProgram program [1]
 
   putStrLn ("Boost result is " ++ show boostResult)
+
+  let distressSignal = runProgram program [2]
+
+  putStrLn ("Distress signal is " ++ show distressSignal)

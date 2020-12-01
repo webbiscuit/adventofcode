a979333251d1cabb46d59b4ba6d85441a63e49bf
Daniel <daniel@webbiscuit.co.uk>
2020-12-01 17:42:12 +0000

    Use Nothing instead of 0 for missing results

--- 2020/src/01/Day01.hs
+++ 2020/src/01/Day01.hs
@@ -4,31 +4,33 @@ module Day01
   find2020TriplesExpense,
 ) where
 
+import Data.Maybe
+
 type Expense = Int
 type Target = Int
 
-find2020Expense :: [Expense] -> Expense
+find2020Expense :: [Expense] -> Maybe Expense
 find2020Expense = findExpense 2020
 
-find2020TriplesExpense :: [Expense] -> Expense
+find2020TriplesExpense :: [Expense] -> Maybe Expense
 find2020TriplesExpense = findTriplesExpense 2020
 
-findExpense :: Target -> [Expense] -> Expense
+findExpense :: Target -> [Expense] -> Maybe Expense
 findExpense t (x:xs)
-  | needle `elem` xs = x * needle
+  | needle `elem` xs = Just $ x * needle
   | otherwise = findExpense t xs
   where
     needle = t - x  
-findExpense _ _ = 0
+findExpense _ _ = Nothing
 
-findTriplesExpense :: Target -> [Expense] -> Expense
+findTriplesExpense :: Target -> [Expense] -> Maybe Expense
 findTriplesExpense t (x:xs)
-  | found > 0 = found * x
+  | Just f <- found =  Just (f * x)
   | otherwise = findTriplesExpense t xs
   where
     needle = t - x  
     found = findExpense needle xs
-findTriplesExpense _ _ = 0
+findTriplesExpense _ _ = Nothing
 
 parseExpenseList :: [String] -> [Expense]
 parseExpenseList = map read
@@ -37,10 +39,10 @@ main :: IO()
 main = do
   input <- getContents
   let expenses = parseExpenseList $ lines input
-  let expenses2020 = find2020Expense expenses
+  let expenses2020 = fromMaybe 0 $ find2020Expense expenses
 
   putStrLn ("Expenses for 2020 as doubles are: " ++ show expenses2020)
 
-  let expenses2020Triples = find2020TriplesExpense expenses
+  let expenses2020Triples = fromMaybe 0 $ find2020TriplesExpense expenses
 
   putStrLn ("Expenses for 2020 as triples are: " ++ show expenses2020Triples)
--- 2020/src/01/test.hs
+++ 2020/src/01/test.hs
@@ -6,27 +6,27 @@ main = hspec $
 
     context "Finding 2020 expense (doubles)" $ do
       it "Example works" $
-        find2020Expense [1721, 979, 366, 299, 675, 1456] `shouldBe` 514579
+        find2020Expense [1721, 979, 366, 299, 675, 1456] `shouldBe` Just 514579
 
-      it "Single item lists return 0" $
-        find2020Expense [1721] `shouldBe` 0
+      it "Single item lists return Nothing" $
+        find2020Expense [1721] `shouldBe` Nothing
 
-      it "Empty lists return 0" $
-        find2020Expense [] `shouldBe` 0
+      it "Empty lists return Nothing" $
+        find2020Expense [] `shouldBe` Nothing
 
-      it "Unmatched lists return 0" $
-        find2020Expense [1234, 456] `shouldBe` 0
+      it "Unmatched lists return Nothing" $
+        find2020Expense [1234, 456] `shouldBe` Nothing
 
     context "Finding 2020 expense (triples)" $ do
       it "Example works" $
-        find2020TriplesExpense [1721, 979, 366, 299, 675, 1456] `shouldBe` 241861950
+        find2020TriplesExpense [1721, 979, 366, 299, 675, 1456] `shouldBe` Just 241861950
 
-      it "Single item lists return 0" $
-        find2020TriplesExpense [1721] `shouldBe` 0
+      it "Single item lists return Nothing" $
+        find2020TriplesExpense [1721] `shouldBe` Nothing
 
-      it "Empty lists return 0" $
-        find2020TriplesExpense [] `shouldBe` 0
+      it "Empty lists return Nothing" $
+        find2020TriplesExpense [] `shouldBe` Nothing
 
-      it "Unmatched lists return 0" $
-        find2020TriplesExpense [1234, 456] `shouldBe` 0
+      it "Unmatched lists return Nothing" $
+        find2020TriplesExpense [1234, 456] `shouldBe` Nothing
       
